c6a16422639778a89563951621bd6b0a925d6fbe
Sergiu Chilat <chilatsergiu@gmail.com>
2021-03-04 14:12:43 +0200

    POO. Mostenire (sb2)

--- POO. Mostenire/app.js
+++ POO. Mostenire/app.js
@@ -1,60 +1,59 @@
 class Employee{
     constructor(){
-        this.name = "Ion Creanga";
-        this.age = 30;
-        this.salary = 5000;
+        this._firstName = "Eminescu";
+        this._lastName = "Mihai";
+        this._age = 22;
+        this._salary = 1000;
     }
 
-    increaseSalary(value){
-        this.salary += value;
+    increaseSalary(percent) {
+        this._salary = this._salary * (percent / 100 + 1);
     }
-
-    decreaseSalary(value){
-        this.salary -= value;
+    
+    decreaseSalary(percent) {
+        this._salary = this._salary * (1 - percent / 100);
     }
 }
+
 
 class Programmer extends Employee{
     constructor(){
         super();
-        this.name = "Ion Creanga";
-        this.age = 30;
-        this.salary = 5000;
+        this._firstName = "Russo";
+        this._lastName = "Alecu";
+        this._age = 22;
+        this._salary = 1000;
     }
 }
 
 class Driver extends Employee{
     constructor(){
         super();
-        this.name = "Ion Creanga";
-        this.age = 30;
-        this.salary = 5000;
+        this._firstName = "Creanga";
+        this._lastName = "Ion";
+        this._age = 22;
+        this._salary = 1000;
     }
 }
 
-
 const employee = new Employee();
-
 console.log(employee);
-
-employee.increaseSalary(500);
-
+employee.increaseSalary(20);
 console.log(employee);
-
-employee.decreaseSalary(700);
-
+employee.decreaseSalary(20);
 console.log(employee);
 
 
 const programmer = new Programmer();
-
 console.log(programmer);
-programmer.increaseSalary(3000);
+programmer.increaseSalary(20);
 console.log(programmer);
-
+programmer.decreaseSalary(20);
+console.log(programmer);
 
 const driver = new Driver();
-
 console.log(driver);
-driver.increaseSalary(5000);
+driver.increaseSalary(20);
+console.log(driver);
+driver.decreaseSalary(20);
 console.log(driver);
